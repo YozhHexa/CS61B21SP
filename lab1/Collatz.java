fce7ec84308c3dee326072fc9fddaf1af0d0b728
Yozh Hexas <231230008@smail.nju.edu.cn>
2025-09-27 20:19:51 +0800

    lab1第一次提交

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,13 +4,20 @@
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
+    /** if n is odd, return 3 * n + 1
+     *  if n is even, return n // 2
+     *  if n is one, over
+     *  the implement of collatz
+     * @param n
+     * @return
+     */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n  == 1) {
             return 1;
-        } else if (n == 5) {
+        } else if (n % 2 == 1) {
             return 3 * n + 1;
         } else {
-            return n * 2;
+            return n / 2;
         }
     }
 
