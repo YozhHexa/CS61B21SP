25bf506709c7fc9b8ee283f23bd950cf2e087a17
Yozh Hexas <231230008@smail.nju.edu.cn>
2025-10-02 12:17:02 +0800

    flik bug fixed

--- lab4/flik/TestFilk.java
+++ lab4/flik/TestFilk.java
@@ -7,6 +7,33 @@ import static org.junit.Assert.*;
 public class TestFilk {
     @Test
     public void testOne(){
+        int a = 128;
+        int b = 128;
+        assertTrue(a == b);
+        return;
+    }
+
+    @Test
+    public void testTwo(){
+        int a = 128;
+        int b = 128;
+        assertTrue(Flik.isSameNumber(a, b));
+        return;
+    }
+
+    @Test
+    public void testThree(){
+        int a = 129;
+        int b = 129;
+        assertTrue(Flik.isSameNumber(a, b));
+        return;
+    }
+
+    @Test
+    public void testFour(){
+        int a = 127;
+        int b = 127;
+        assertTrue(Flik.isSameNumber(a, b));
         return;
     }
 }
